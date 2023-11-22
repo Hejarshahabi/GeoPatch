739e58b460e8a985ad9e85c56b42c69da29f69a0
Hejar Shahabi <hejarshahabi@gmail.com>
2023-11-22 17:22:23 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from setuptools import setup, find_packages
 
 with open("ReadMe.md", "r", encoding="utf-8") as ReadMe:
     long_description = ReadMe.read()
+
 classifiers = [
     'Development Status :: 5 - Production/Stable',
     'Intended Audience :: Science/Research',
@@ -12,7 +13,7 @@ classifiers = [
 
 setup(
     name='GeoPatch',
-    version='1.0.6',
+    version='1.1.1',
     description='GeoPatch is a package for generating patches from remote sensing data',
     long_description=long_description,
     url='https://github.com/Hejarshahabi',
@@ -20,9 +21,10 @@ setup(
     author_email='hejarshahabi@gmail.com',
     license='MIT',
     classifiers=classifiers,
-    keywords='Machine Learning, Remote Sensing' 'Deep Learning',
+    keywords='Machine Learning, Remote Sensing, Deep Learning',
     long_description_content_type='text/markdown',
-    packages=find_packages( ),
-    install_requires=['numpy','pandas','matplotlib','sklearn','rasterio','tqdm']
+    packages=find_packages(),
+    data_files=[('', ['GDAL-3.2.3-cp38-cp38-win_amd64.whl'])],
+    install_requires=['numpy', 'pandas', 'matplotlib', 'scikit-learn', 'rasterio', 'tqdm'],
+    
 )
-
