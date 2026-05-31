6d49e7077b14d1d15dddd5153a3e2581e559122e
hejar shahabi <hejarshahabi@gmail.com>
2026-05-31 01:29:36 -0400

    Fix logo URL for PyPI compatibility and bump to 1.2.5

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ classifiers = [
 
 setup(
     name='GeoPatch',
-    version='1.2.4',
+    version='1.2.5',
     description='GeoPatch is a package for generating patches from remote sensing data',
     long_description=long_description,
     url='https://github.com/Hejarshahabi',
