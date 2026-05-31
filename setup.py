e988922d119e86412f5190053ae456fcc854c301
hejar shahabi <hejarshahabi@gmail.com>
2026-05-31 01:32:53 -0400

    Move badges above logo and bump to 1.2.6

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ classifiers = [
 
 setup(
     name='GeoPatch',
-    version='1.2.5',
+    version='1.2.6',
     description='GeoPatch is a package for generating patches from remote sensing data',
     long_description=long_description,
     url='https://github.com/Hejarshahabi',
